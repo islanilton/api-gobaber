c14d152839d360cd881232ee26042d6f46da7f9d
Islanilton <islanilton.rodrigues@gmail.com>
2020-10-05 10:32:39 -0300

    add code pattern

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
-app.listen(3333, () => console.log('Server started on port 3333'))
+app.listen(3333, () => console.log('Server started on port 3333'));
