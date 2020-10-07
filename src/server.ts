fa3d13637ddcece3e261e5b60f5731e13d250ca5
Islanilton <islanilton.rodrigues@gmail.com>
2020-10-07 08:56:49 -0300

    add date-fns

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(express.json());
+app.use('/api', routes);
 
 app.listen(3333, () => console.log('Server started on port 3333'));
