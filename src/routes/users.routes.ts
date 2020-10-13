2e7db3324b1e1c5bba1924ef68c024458c873347
Islanilton <islanilton.rodrigues@gmail.com>
2020-10-13 09:00:58 -0300

    add hashed password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 
+import User from '../models/User';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
@@ -9,10 +11,24 @@ usersRouter.post('/', async (request, response) => {
     const { name, email, password } = request.body;
     const createUser = new CreateUserService();
     const user = await createUser.execute({ name, email, password });
-    return response.json(user);
+    const { id, created_at, updated_at } = user;
+    return response.json({
+      id,
+      name,
+      email,
+      created_at,
+      updated_at,
+    });
   } catch (error) {
     return response.status(400).json({ message: error.message });
   }
+});
+
+usersRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+  const usersRepository = getRepository(User);
+  await usersRepository.delete(id);
+  return response.send();
 });
 
 export default usersRouter;
